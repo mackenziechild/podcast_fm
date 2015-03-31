f02aa83e801efaa53a30fa14a60a57c07e0242c4
Mackenzie Child <mackenzie.child@gmail.com>
2015-03-31 00:19:16 -0600

    Added dashboard for user

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,10 +1,28 @@
 class PodcastsController < ApplicationController
+	before_action :find_podcast, only: [:show, :dashboard]
+	before_action :find_episode, only: [:show, :dashboard]
+
 	def index
 		@podcasts = Podcast.all.order("created_at DESC")
 	end
 
 	def show
-		@podcast = Podcast.find(params[:id])
+	end
+
+	def dashboard
+	end
+
+	private
+
+	def find_episode
 		@episodes = Episode.where(podcast_id: @podcast).order("created_at DESC")
+	end
+
+	def find_podcast
+		if params[:id].nil?
+			@podcast = current_podcast
+		else
+			@podcast = Podcast.find(params[:id])
+		end
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
 
   devise_for :podcasts
-  root 'welcome#index'
 
   resources :podcasts, only: [:index, :show] do
   	resources :episodes
   end
+
+  authenticated :podcast do
+  	root 'podcasts#dashboard', as: "authenticated_root"
+  end
+
+  root 'welcome#index'
 end
