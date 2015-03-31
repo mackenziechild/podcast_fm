9dd1f7db7d1be09737a01842adea0bb6c2e4d90c
Mackenzie Child <mackenzie.child@gmail.com>
2015-03-31 00:29:41 -0600

    Add edit & destroy ability to episodes

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,6 +1,6 @@
 class EpisodesController < ApplicationController
 	before_action :find_podcast
-	before_action :find_episode, only: [:show]
+	before_action :find_episode, only: [:show, :edit, :update, :destroy]
 
 	def new
 		@episode = @podcast.episodes.new
@@ -17,6 +17,22 @@ class EpisodesController < ApplicationController
 
 	def show
 		@episodes = Episode.where(podcast_id: @podcast).order("created_at DESC").reject { |e| e.id == @episode.id }
+	end
+
+	def edit
+	end
+
+	def update
+		if @episode.update episode_params
+			redirect_to podcast_episode_path(@podcast, @episode), notice: "Episode was succesfully updated!"
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@episode.destroy
+		redirect_to root_path
 	end
 
 	private
